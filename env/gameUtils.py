2744ad362a37d24ed14f02b66722501ddfb164ea
zhangheng <1049725878@qq.com>
2021-09-12 23:50:21 +0800

    'a'

---
+++ env/gameUtils.py
@@ -0,0 +1,24 @@
+type_error = 0x0   # 非法牌型
+type_one = 0x1000  # 单张
+type_two = 0x2000  # 一对
+type_three = 0x3000  # 三不带
+
+type_one_continuity_five = 0x4000  # 五单连
+type_one_continuity_six = 0x5000  # 六单连
+type_one_continuity_seven = 0x6000  # 7 单
+type_one_continuity_eight = 0x7000  # 8 单
+type_one_continuity_nine = 0x8000  # 9 单
+type_one_continuity_ten = 0x9000  # 10 单
+type_one_continuity_eleven = 0xA000  # 11 单
+type_one_continuity_twelve = 0xB000  # 12 单
+
+type_two_continuity_three = 0xC000  # 3 双
+type_two_continuity_four = 0xD000  # 4 双
+type_two_continuity_five = 0xE000  # 5 双
+type_two_continuity_six = 0xF000  # 6 双
+type_two_continuity_seven = 0x10000  # 7 双
+type_two_continuity_eight = 0x20000  # 8 双
+type_two_continuity_nine = 0x10000  # 9 双
+type_two_continuity_ten = 0x20000  # 10 双  不会有这么好的运气吧
+
+
