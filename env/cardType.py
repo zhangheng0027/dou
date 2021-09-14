554880a6a0ba3746b3e5c33981599b3e1e5e43e9
zhangheng0027 <33078117+zhangheng0027@users.noreply.github.com>
2021-09-14 19:18:40 +0800

    'a'

--- env/cardType.py
+++ env/cardType.py
@@ -1,3 +1,9 @@
+import os
+import pickle
+from typing import List
+
+from collections import Counter
+
 type_error = 0x0  # 非法
 type_one = 0x1000  # 单张
 type_two = 0x2000  # 一对
@@ -73,5 +79,170 @@ def encode(a):
     return "type_error", 0
 
 
+t = {}
+
+
+def selectCardType(cs: List):
+    """
+    判断牌的类型
+    :param cs:
+    :return:
+    """
+    global t
+    cs.sort()
+    if len(t) == 0:
+        if os.path.exists('CardType.tmp'):
+            t = pickle.load(open('CardType.tmp', 'rb'))
+        else:
+            t = generateCardType()
+            with open('CardType.tmp', 'wb') as g:
+                pickle.dump(t, g)
+    st = ''.join(str(i) for i in cs)
+    if t.__contains__(st):
+        return t[st]
+
+    length = len(cs)
+    if length < 4:
+        return type_error
+    aa = Counter(cs).most_common(10)
+    if 4 == length:
+        # 三带一的情况
+        (ma, cou) = aa[0]
+        if 3 == cou:
+            return ma + type_three_with_one
+        return type_error
+
+    if 5 == length:
+        # 3 带 2
+        (ma, cou) = aa[0]
+        if 3 == cou:
+            return ma + type_three_with_two
+        return type_error
+
+    if 6 == length:
+        # 4 带二张
+        (ma, cou) = aa[0]
+        if 4 == cou:
+            return ma + type_four_with_two
+        return type_error
+
+    if 8 == length:
+        (a, b) = aa[0]
+        if 4 == b:
+            # 4 带 2 对
+            if len(aa) == 3 and aa[1][1] == 2 and aa[2][1] == 2:
+                return a + type_four_with_two_pair
+            return type_error
+        else:
+            # 飞机
+            if len(aa) > 2 and b == aa[1][1] == 3 and aa[1][0] == a + 1:
+                return a + 1 + type_three_continuity_two_with_one
+            return type_error
+
+    if 10 == length:
+        if len(aa) == 4 and 3 == aa[0][1] == aa[1][1] and aa[1][0] == aa[0][0] + 1 \
+                and aa[2][1] == aa[3][1]:
+            return aa[1][0] + type_three_continuity_two_with_two
+        return type_error
+
+    if 12 == length:
+        # 3 飞 带 1
+        if len(aa) > 3 and 3 == aa[0][1] == aa[2][1] and aa[0][0] + 2 == aa[2][0]:
+            return aa[2][0] + type_three_continuity_three_with_one
+        return type_error
+
+    if 15 == length:
+        # 3 飞 带 2
+        if len(aa) == 6 and 3 == aa[0][1] == aa[2][1] and aa[0][0] + 2 == aa[2][0]\
+                and 2 == aa[5][1]:
+            return aa[2][0] + type_three_continuity_three_with_two
+        return type_error
+
+    if 16 == length:
+        # 4 飞 带 1
+        if len(aa) > 4 and 3 == aa[0][1] == aa[3][1] \
+                and aa[0][0] + 3 == aa[3][0]:
+            return aa[3][0] + type_three_continuity_four_with_one
+        return type_error
+
+    if 20 == length:
+        if len(aa) == 8 and 3 == aa[0][1] == aa[3][1] and aa[0][0] + 3 == aa[3][0] \
+                and 2 == aa[4][1] == aa[7][1]:
+            return aa[3][0] + type_three_continuity_four_with_two
+        else:
+            if len(aa) > 5 and 3 == aa[0][1] == aa[4][1] and aa[0][0] + 4 == aa[4][0]:
+                return aa[4][0] + type_three_continuity_five_with_one
+
+    return type_error
+
+
+def generateCardType():
+    """
+    对基础牌进行缓存
+    :return:
+    """
+    temp = {}
+    generateCardType_one(temp)
+    generateCardType_two(temp)
+    generateCardType_other(temp)
+    generateCardType_three(temp)
+    return temp
+
+
+def generateCardType_one(t: {}):
+    """
+    生成单连
+    :param t:
+    :return:
+    """
+    cou = type_one_continuity_five
+    for i in range(5, 15):
+        for j in range(3, 16 - i):
+            ll = ''.join(['%s' % z for z in range(j, i + j)])
+            t[ll] = cou + i + j - 1
+        cou = cou + 0x1000
+
+
+def generateCardType_two(t: {}):
+    """
+    生成双连
+    :param t:
+    :return:
+    """
+    cou = type_two_continuity_three
+    for i in range(3, 11):
+        for j in range(3, 16 - i):
+            ll = ''.join(['%s%s' % (z, z) for z in range(j, i + j)])
+            t[ll] = cou + i + j - 1
+        cou += 0x1000
+
+
+def generateCardType_three(t: {}):
+    """
+    生成飞机
+    :param t:
+    :return:
+    """
+    cou = type_three_continuity_two
+    for i in range(2, 7):
+        for j in range(3, 16 - i):
+            ll = ''.join(['%s%s%s' % (z, z, z) for z in range(j, i + j)])
+            t[ll] = cou + i + j - 1
+        cou += 0x1000
+
+
+def generateCardType_other(t: {}):
+    for i in range(3, 18):
+        t[str(i)] = type_one + i
+
+    for i in range(3, 16):
+        t[str(i) * 2] = type_two + i
+        t[str(i) * 3] = type_three + i
+        t[str(i) * 4] = type_boom + i
+
+    t['1617'] = type_boom + 17
+
+
 if __name__ == '__main__':
-    print(encode(0x10003))
+    aa = selectCardType([3,4,5,6,7,8,9])
+    print(encode(aa))
