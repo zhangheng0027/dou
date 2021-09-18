96737a2f26a21cf51837c375984c199814126449
zhangheng0027 <33078117+zhangheng0027@users.noreply.github.com>
2021-09-18 09:43:58 +0800

    '效率优化'

--- env/gameUtils.py
+++ env/gameUtils.py
@@ -85,7 +85,7 @@ def _selectCount(arr: List):
         for i in range(index - cou + 1, index + 1):
             ar[i] += de
 
-    def oneCount(ar: []):
+    def oneCount(ar: [], mc=54):
         index, cou = selectOne(ar)
         if index < 0:
             c = 0
@@ -93,16 +93,23 @@ def _selectCount(arr: List):
                 if v > 0:
                     c += 1
             return c
-        c = oneCount(ar)
-        if c <= 1:
-            recovery(ar, index, cou, 1)
-            return c + 1
+        # c = oneCount(ar)
+        # if c <= 1:
+        #     recovery(ar, index, cou, 1)
+        #     return c + 1
+        rightCount = oneCount(ar[index + 1:])
+        f, leftCount = splitCard(ar, oneCount, mc)
+        if f:
+            leftCount = leftCount - rightCount
         for _ in range(0, cou - 5):
             ar[index] += 1
             index -= 1
-            c = min(c, oneCount(ar))
+            f, cc = splitCard(ar, oneCount, mc)
+            if f:
+                cc = cc - cc
+            leftCount = min(cc, leftCount)
         recovery(ar, index, 5, 1)
-        return c + 1
+        return leftCount + rightCount + 1
 
     def countTwo(ar: [], minC):
         index, cou = selectTwo(ar)
@@ -112,16 +119,24 @@ def _selectCount(arr: List):
             recovery(ar, index, cou, 2)
             return 53
 
-        d = min(minC, oneCount(ar))
-        d = min(d, countTwo(ar, d - 1))
+        # d = min(minC, oneCount(ar))
+        # d = min(d, countTwo(ar, d - 1))
+
+        rightCount = countTwo(ar[index + 1:], minC - 1)
+        f, leftCount = splitCard(ar, countTwo, minC - 1)
+        if f:
+            leftCount = leftCount - rightCount
 
         for _ in range(0, cou - 3):
             ar[index] += 2
             index -= 1
-            d = min(d, oneCount(ar))
-            d = min(d, countTwo(ar, d - 1))
+            f, cc = splitCard(ar, countTwo, minC - 1)
+            if f:
+                cc = cc - rightCount
+            leftCount = min(leftCount, cc)
+
         recovery(ar, index, 3, 2)
-        return d
+        return leftCount + rightCount + 1
 
     def countThree(ar: [], minC):
         index, cou = selectThree(ar)
@@ -130,11 +145,22 @@ def _selectCount(arr: List):
         if minC < 0:
             return 53
         rightCount = countThree(ar[index + 1:], minC - 1)
-        leftCount = countThree(ar[:index + 1 - cou], minC - 1) - 0.49 * cou  # 3 带一 相当于多出 0.5 手
+
+        f, leftCount = splitCard(ar, countThree, minC - 1)
+        leftCount -= 0.49 * cou
+        if f:
+            leftCount = leftCount - rightCount
+
+        # leftCount = countThree(ar[:index + 1 - cou], minC - 1) - 0.49 * cou  # 3 带一 相当于多出 0.5 手
         for i in range(index + 1 - cou, index):
             ar[i] += 3
             cou -= 1
-            leftCount = min(leftCount, countThree(ar[: i + 1], minC - 1) - 0.49 * cou)
+            # leftCount = min(leftCount, countThree(ar[: i + 1], minC - 1) - 0.49 * cou)
+            f, cc = splitCard(ar, countThree, minC - 1) - 0.49 * cou
+            cc -= 0.49 * cou
+            if f:
+                cc = cc - rightCount
+            leftCount = min(leftCount, cc)
         ar[index] += 3
         return min(1 + rightCount + leftCount, countTwo(ar, minC))
 
@@ -147,12 +173,21 @@ def _selectCount(arr: List):
         m = countFour(ar[index + 1:], minC - 1) + countFour(ar[:index], minC - 1) - 1.009  # 4 个相当于多出 1 手
         ar[index] += 4
         return min(m, countThree(ar, minC))
+
+    def splitCard(ar: [], ff, minC):
+        for index, val in enumerate(ar):
+            if val == 0:
+                return False, ff(ar[:index + 1], minC)
+        return True, ff(ar, minC)
+
     return countFour(arr, oneCount(arr))
 
 
 if __name__ == '__main__':
-    cs = game.generateNewCard()
-    print(cs['landlord'])
-    print(selectCount(cs['landlord']))
-    # a = [3, 5, 6, 6, 6, 7, 8, 8, 9, 9, 10, 11, 12, 13, 13, 14, 14, 15, 15, 16]
-    # print(selectCount(a))
+    # cs = game.generateNewCard()
+    # print(cs['landlord'])
+    # print(cs['landlord'], selectCount(cs['landlord']))
+    # print(cs['landlord_up'], selectCount(cs['landlord_up']))
+    # print(cs['landlord_down'], selectCount(cs['landlord_down']))
+    a = [8, 8, 8, 9, 9, 10, 10, 11, 11, 11, 12, 13, 13, 14]
+    print(selectCount(a))
