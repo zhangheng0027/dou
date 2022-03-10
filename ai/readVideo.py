7fa1ad2746e008dc2e9866d3f5ff4e820cb7b21f
zhangheng0027 <33078117+zhangheng0027@users.noreply.github.com>
2022-03-10 20:24:43 +0800

    aa

--- ai/readVideo.py
+++ ai/readVideo.py
@@ -1,22 +1,29 @@
+# import cv2
 import cv2
 
 cap = cv2.VideoCapture('E:\\test\\1.mp4')
+
 
 if cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         exit
-    p1 = frame[144:144+21, 27:27 + 21]
-    cv2.imwrite("E:/test/picture/p1.jpg", p1)
 
+    f1 = cv2.cvtColor(frame[135:135+24, 21:21+28], cv2.COLOR_RGB2GRAY)
+    ret, binary = cv2.threshold(f1, 185, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("image", binary)
+
+    # #
     p2 = frame[139:139 + 21, 687:687 + 21]
-    cv2.imwrite("E:/test/picture/p2.jpg", p2)
-
-    img_gray = cv2.cvtColor(p2, cv2.COLOR_RGB2GRAY)
-
-    cv2.imwrite("E:/test/picture/p1_2.jpg", img_gray)
-
-    # cv2.imshow("img_gray", img_gray)
-    print("success")
+    f2 = cv2.cvtColor(p2, cv2.COLOR_RGB2GRAY)
+    ret, binary = cv2.threshold(f2, 160, 255, cv2.THRESH_BINARY)
+    cv2.imshow("image1", binary)
+    cv2.waitKey(0)
+    #
+    # cv2.imwrite("E:/test/picture/p1_2.jpg", img_gray)
+    #
+    # # cv2.imshow("img_gray", img_gray)
+    # print("success")
 else:
     print("读取失败")
